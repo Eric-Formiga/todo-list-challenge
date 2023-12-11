5e6420ca0b38093aa7492bda300cb0612de21856
Eric-Formiga <eric.13formiga@gmail.com>
2023-12-11 07:55:05 -0300

    feat: add task sctructure and screen

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <>
       {fontsLoaded ? <HomeScreen/> : <Loading/>}
-      <StatusBar style="auto" />
+      <StatusBar />
     </>
   );
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,20 @@
 import { Text, View } from 'react-native'
 import { styles } from './styles'
 import { Header } from '../components/Header/Header'
+import { Task } from '../components/Task/Task'
 
 export function HomeScreen() {
   return (
     <View style={
       styles.container
     } >
-    <Header/>
+      <Header />
+      <View style={styles.tasksContainer}>
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+      </View>
     </View>
   )
 }
