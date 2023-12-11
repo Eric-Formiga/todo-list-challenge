5830f5e91ba63c24cfe5d1e3aeb5e084d86c8eeb
Eric-Formiga <eric.13formiga@gmail.com>
2023-12-11 08:28:53 -0300

    feat: add Empty component

---
+++ src/components/Empty/Empty.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+import { theme } from "../../theme";
+
+export function Empty(){
+  return <View style={styles.emptyContainer}>
+    <Text style={styles.textBold}>Você ainda não tem tarefas cadastradas</Text>
+    <Text style={[styles.textBold, styles.textRegular]}>Crie suas Tarefas</Text>
+  </View>
+}
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,19 +1,30 @@
-import { Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import { styles } from './styles'
 import { Header } from '../components/Header/Header'
 import { Task } from '../components/Task/Task'
+import { useState } from 'react'
+import { TaskDTO } from '../dtos/TaskDto'
+import { Empty } from '../components/Empty/Empty'
 
 export function HomeScreen() {
+  const [tasks, settasks] = useState<TaskDTO[]>([
+  ])
   return (
-    <View style={
-      styles.container
-    } >
+    <View style={styles.container} >
       <Header />
       <View style={styles.tasksContainer}>
-        <Task />
-        <Task />
-        <Task />
-        <Task />
+
+        <FlatList
+        data={tasks}
+        keyExtractor={(tasks)=> tasks.id!}
+        renderItem={({item}) =>  
+        <Task key={item.id} 
+          title={item.title} 
+          isCompleted={item.isCompleted}
+           />}
+           ListEmptyComponent={<Empty/>}
+        />
+
       </View>
     </View>
   )
