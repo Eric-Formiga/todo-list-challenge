f6a3b892e366d30e7367b102b870cb46accd146e
Eric-Formiga <eric.13formiga@gmail.com>
2023-12-11 00:08:54 -0300

    feat: created project structure

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from 'react-native'
+import { styles } from './styles'
+
+export function HomeScreen() {
+  return (
+    <View style={
+      styles.container
+    } >
+      <Text style={styles.title}>Home</Text>
+    </View>
+  )
+}
+
+
+
+
